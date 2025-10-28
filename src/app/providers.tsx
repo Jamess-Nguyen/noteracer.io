a8f7687cb889625c62f1948bb7b27c708e9a3d8d
James Nguyen <haimenguyen@gmail.com>
2025-10-27 19:34:24 -0700

    :pencil2: renamed provider to qcprovider wrapper

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
-export function Provider({children}: any){
+export function QcProvider({children}: any){
   const [qc] = useState(() => {
     return new QueryClient();
   });
