05da95d4d752d298baaa6a0ca98fe1ad864e5dc6
James Nguyen <haimenguyen@gmail.com>
2025-11-02 17:55:29 -0800

    :sparkles: added state mangewr for gameplay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { GoogleSignIn } from "./components/auth/GoogleSignIn";
 import { TwoItemNavBar } from "./components/navigation/TwoItemNavBar";
-import { GamePlayHeader } from "./features/gameplay/components/GamePlayHeader";
 
 export default function Home() {
   return (
