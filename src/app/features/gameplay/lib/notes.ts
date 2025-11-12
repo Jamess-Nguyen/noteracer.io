f65b03dc1d8a703c7d7b13f13316f2157e2272bb
jamess-nguyen <haimenguyen@gmail.com>
2025-11-11 17:12:06 -0800

    :art: added more notes to public

--- src/app/features/gameplay/lib/notes.ts
+++ src/app/features/gameplay/lib/notes.ts
@@ -6,6 +6,16 @@ export function generateNotes(): Array<string>{
     roundNotes[i] = note;
 
   }
+  roundNotes[0] = 'e';
+  roundNotes[1] = 'g';
+  roundNotes[2] = 'b';
+  roundNotes[3] = 'd';
+  roundNotes[4] = 'f';
+  roundNotes[5] = 'f';
+  roundNotes[6] = 'd';
+  roundNotes[7] = 'e';
+  roundNotes[8] = 'g';
+  roundNotes[9] = 'b';
 
   return roundNotes;
 }
