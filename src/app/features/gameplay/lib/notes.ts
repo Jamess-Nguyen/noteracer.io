b1e5cf21deee9a44e496f0f022dd16f0ec89b5f2
James Nguyen <haimenguyen@gmail.com>
2025-11-11 21:32:13 -0800

    :art: added colors to indicate note state

--- src/app/features/gameplay/lib/notes.ts
+++ src/app/features/gameplay/lib/notes.ts
@@ -1,5 +1,5 @@
 export function generateNotes(count = 10): string[] {
-  const notes = ['e', 'g', 'b', 'd', 'f'];
+  const notes = ['a', 'c', 'e', 'g', 'b', 'd', 'f'];
   const randomNotes = Array.from({ length: count }, () => notes[Math.floor(Math.random() * notes.length)]);
   return randomNotes;
 }
